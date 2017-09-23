4bbf853b53562c5fc30e6d82921f5a04a92ec384
msaxton <msaxton@iliff.edu>
2017-09-23 13:54:52 -0600

    Working on assignment

--- Assignment1.R
+++ Assignment1.R
@@ -1,1 +1,7 @@
 my.name <- "emily"
+my.age <- 24
+MakeIntroduction <- function(age, name) {
+  return(paste("Hello, my name is", name, "and I'm", age, "years old."))
+}
+my.intro <- MakeIntroduction(age=my.age, name=my.name)
+print(my.intro)
